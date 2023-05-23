24bf3dabf6786f91a0f8dcce16596c2843d7c88d
Lee Dong Hun <dlehdgns960@naver.com>
2023-05-23 14:47:03 +0900

    commit 2 계산기에 빼기함수 추가

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ def add_func(n1,n2) :
     result =  n1+n2
     return result
 
+def sub_func(n1,n2) :
+    result = n1 - n2
+    return result
 
 ## 전역 변수부(인스턴스 변수)
 num1,num2,res = 100, 200, 0
@@ -12,3 +15,6 @@ num1,num2,res = 100, 200, 0
 ## 메인 코드부(메인 메서드)
 res = add_func(num1,num2)
 print(num1,'+',num2,'=',res)
+
+res = sub_func(num1,num2)
+print(num1,'-',num2,'=',res)
